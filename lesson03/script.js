505ee5e10b357b6e7de2fdbd6fa4a017ef069eee
Konstantin Pozharitskiy <tripless@mail.ru>
2022-10-04 17:48:19 +0300

    fixed bags

--- lesson03/script.js
+++ lesson03/script.js
@@ -2,34 +2,42 @@
 
 // 1). Переменная lang может принимать 2 значения: 'ru' 'en'.
 let lang = prompt('Выберите язык - введите ru или en');
+const langArray = [["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+["Monday", "Tuesday", "Wednesday", "Thursdey", "Friday", "Saturday", "Sunday"]];
 
 // Написать условия при котором в зависимости от значения lang будут выводится 
 //дни недели на русском или английском языке.
 //Решите задачу:
 
+const getArrayElements = (array) => {
+  let result = '';
+  for (let i = 0; i < array.length; i++) {
+    result += array[i] + ', ';
+  }
+  return result.slice(0, -2);
+};
+
 //  a) через if,
-if(lang =='ru'){
-  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else{
-  console.log('Monday, Tuesday, Wednesday, Thursdey, Friday, Saturday, Sunday');
+if (lang == 'ru') {
+  console.log(getArrayElements(langArray[0]));
+} else {
+  console.log(getArrayElements(langArray[1]));
 }
 
 //  b) через switch-case
 
-switch(lang){
+switch (lang) {
   case 'ru':
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    console.log(getArrayElements(langArray[0]));
     break;
   case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursdey, Friday, Saturday, Sunday');
+    console.log(getArrayElements(langArray[1]));
     break;
 }
 
 //  c) через многомерный массив без ифов и switch.
-let langArray = [];
-langArray.ru = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-langArray.en = 'Monday, Tuesday, Wednesday, Thursdey, Friday, Saturday, Sunday';
-console.log(langArray[lang]);
+
+console.log(lang === 'ru' ? getArrayElements(langArray[0]) : console.log(getArrayElements(langArray[1])));
 
 // 2). У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”,
 // если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
@@ -37,6 +45,6 @@ console.log(langArray[lang]);
 //  Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
 let namePerson = prompt("Введите имя:");
 let result = namePerson === "Артем" ? console.log("директор") :
- (namePerson === "Александр") ? console.log("преподаватель") : 
- console.log("студент");
+  (namePerson === "Александр") ? console.log("преподаватель") :
+    console.log("студент");
 
