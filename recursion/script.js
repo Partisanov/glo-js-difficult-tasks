d2baee77c0b2265ebde9c4e1b7a6cf54d806cdfa
Konstantin Pozharitskiy <tripless@mail.ru>
2022-10-28 20:50:49 +0300

    returned deleted folders

---
+++ recursion/script.js
@@ -0,0 +1,21 @@
+'use strict';
+//блок объявления переменных 
+
+//блок описания функций 
+const decorateText = () => {
+    let colorList = ["#009900", "#0099990", "#003399", "#663399", "#CC3399"];
+    const pWrapper = document.getElementById('paragraphs-wrapper');
+    const paragraphs = pWrapper.getElementsByTagName('p');
+    let colorNumber = 0;
+
+    for (let i = 0; i < paragraphs.length; i++) {
+        const paragraph = paragraphs.item(i);
+        paragraph.style.color = (colorList[colorNumber]);
+        colorNumber = (colorNumber === (colorList.length - 1)) ? 0 : ++colorNumber;
+    }
+};
+
+//блок функционала
+decorateText();
+
+// блок вывода в консоль
