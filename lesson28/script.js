ca0e6feb886c26914b6cce41bd7ce8d1a0a39a25
Konstantin Pozharitskiy <tripless@mail.ru>
2022-11-10 00:18:55 +0300

    add preloader to summit button

--- lesson28/script.js
+++ lesson28/script.js
@@ -2,6 +2,7 @@ const myHeaders = new Headers();
 myHeaders.append("apikey", "CZH8qJealtsViSl2N9NicLvlEKNRirA7");
 const inputAmount = document.getElementById('input');
 const form = document.getElementById('form');
+const spinner = document.getElementById('spinner');
 
 const requestOptions = {
     method: 'GET',
@@ -15,12 +16,17 @@ const getResult = () => {
     const toValue = document.getElementById('to').value;
     const resultInput = document.getElementById('result');
     if (!amount) {
+        spinner.style.display = "none";
         return;
     }
+
     fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${toValue}&from=${fromValue}&amount=${amount}`,
         requestOptions)
         .then(response => response.json())
-        .then(result => resultInput.value = (result.result).toFixed(2))
+        .then(result => {
+            spinner.style.display = "none";
+            resultInput.value = (result.result).toFixed(2);
+        })
         .catch(error => console.log('error', error));
 };
 
@@ -31,14 +37,12 @@ inputAmount.addEventListener('input', (e) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     try {
+        spinner.style.display = "inline-block";
         getResult();
     } catch (error) {
         console.log(error.message);
     }
 
-
-
-
-
 });
+
 
